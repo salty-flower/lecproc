c4a6035eaaf522b79218fc6c910fc89ab53523a6
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-10-09 13:29:53 +0800

    feat: switch to large-v3-turbo

--- src/audio2text/settings.py
+++ src/audio2text/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: str = "large-v3"
+    model_path: str = "deepdml/faster-whisper-large-v3-turbo-ct2"
     compute_type: str = "default"
 
 
